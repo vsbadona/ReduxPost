f522ecc51ad9368b639153f58b5fdd849737294d
Vishal-badona1 <68052615+Vishal-badona1@users.noreply.github.com>
2023-01-14 22:20:17 +0530

    Changed Database Url And Nodemon To Node

--- server/server.js
+++ server/server.js
@@ -6,20 +6,30 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT
-const url = process.env.DATABASE_URL
+const PORT = process.env.PORT || 5000
+const uri = process.env.DATABASE_URL
 
 app.use(express.json())
 app.use(cors())
 app.use('/',postsRoutes)
 
-app.listen(PORT,()=>console.log(`App is working on http://localhost:${PORT}`))
 
 
 //                                   Database Section                                  //
 
-mongoose.connect(url)
-mongoose.set('strictQuery', true);
-const db = mongoose.connection
-db.on('error',(err)=>console.log("Can't Connect To Db"))
-db.once('open',()=>console.log("Connected To Db"))
+
+
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`server running on http://localhost:${PORT}`)
+    )
+  }
+  )
+  .catch((err) => console.log(err + " can't connect to db"));
