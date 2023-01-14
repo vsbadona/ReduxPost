c0f570382ce290f2207c4e55538129f26fee97bd
Vishal-badona1 <68052615+Vishal-badona1@users.noreply.github.com>
2023-01-14 22:48:22 +0530

    Changed Server.js

--- server/server.js
+++ server/server.js
@@ -1,29 +1,26 @@
 import express from "express";
 import postsRoutes from "./routes/postsRoutes.js";
 import cors from "cors"
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import dotenv from "dotenv"
 dotenv.config()
 
-const app = express()
 const PORT = process.env.PORT || 5000
+const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use('/', postsRoutes)
+app.listen(PORT,()=>console.log(`App Is Listening On http://localhost:${PORT}`))
 
 //                                   Database Section                                  //
 
 mongoose.set("strictQuery", true);
 mongoose
-    .connect(process.env.DATABASE_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        app.listen(PORT, () =>
-            console.log(`server running on http://localhost:${PORT}`)
-        )
-    }
-    )
-    .catch((err) => console.log(err + " can't connect to db"));
+.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+const db = mongoose.connection
+db.on('error',(err)=>console.log(err,"Can't Connect To Db"))
+db.once('open',()=>console.log("Connected To Db"))
