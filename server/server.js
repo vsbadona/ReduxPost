1bb82924b31f92343811c64586c4a2647dc9feb7
Vishal-badona1 <68052615+Vishal-badona1@users.noreply.github.com>
2023-01-14 23:24:33 +0530

    Changed Database Ul

--- server/server.js
+++ server/server.js
@@ -3,11 +3,13 @@ import postsRoutes from "./routes/postsRoutes.js";
 import cors from "cors"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import bodyParser from "body-parser";
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () =>
 console.log(`Connected To Server With Port ${PORT}`)
 )
