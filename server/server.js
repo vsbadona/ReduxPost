a24424778838df3deeea89803a236a0a4c335ef6
Vishal-badona1 <68052615+Vishal-badona1@users.noreply.github.com>
2023-01-14 23:15:52 +0530

    Changed Database Ul

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use('/', postsRoutes)
-app.listen(PORT,()=>console.log(`App Is Listening On http://localhost:${PORT}`))
 
 //                                   Database Section                                  //
 
