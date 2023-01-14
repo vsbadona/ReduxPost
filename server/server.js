33a012916a70ddd84517deca0122e1f3b52f2fdb
Vishal-badona1 <68052615+Vishal-badona1@users.noreply.github.com>
2023-01-14 23:19:19 +0530

    Changed Database Ul

--- server/server.js
+++ server/server.js
@@ -6,8 +6,11 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
-const app = express()
 
+const app = express()
+app.listen(PORT, () =>
+console.log(`Connected To Server With Port ${PORT}`)
+)
 app.use(express.json())
 app.use(cors())
 app.use('/', postsRoutes)
@@ -20,9 +23,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
-    app.listen(PORT, () =>
-        console.log(`server running on http://localhost:${PORT}`)
-    )
+        console.log(`Connected To Db`)
 }
 )
 .catch((err) => console.log(err + " can't connect to db"));
