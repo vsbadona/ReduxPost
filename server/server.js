370df836092d9686eb44079b7ca0718c663eeccf
Vishal-badona1 <68052615+Vishal-badona1@users.noreply.github.com>
2023-01-14 22:36:43 +0530

    Changed Some Lines In Database Url

--- server/server.js
+++ server/server.js
@@ -10,25 +10,20 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
-app.use('/',postsRoutes)
-
-
+app.use('/', postsRoutes)
 
 //                                   Database Section                                  //
 
-
-
-
 mongoose.set("strictQuery", true);
 mongoose
-  .connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () =>
-      console.log(`server running on http://localhost:${PORT}`)
+    .connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () =>
+            console.log(`server running on http://localhost:${PORT}`)
+        )
+    }
     )
-  }
-  )
-  .catch((err) => console.log(err + " can't connect to db"));
+    .catch((err) => console.log(err + " can't connect to db"));
