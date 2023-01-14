a98deb485a66eb02be4a032c564f61ccdc0f0899
Vishal-badona1 <68052615+Vishal-badona1@users.noreply.github.com>
2023-01-14 23:29:28 +0530

    Changed Server.js File

--- server/server.js
+++ server/server.js
@@ -7,15 +7,10 @@ import bodyParser from "body-parser";
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
-
 const app = express()
-app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () =>
 console.log(`Connected To Server With Port ${PORT}`)
 )
-app.use(express.json())
-app.use(cors())
-app.use('/', postsRoutes)
 
 //                                   Database Section                                  //
 
@@ -24,8 +19,12 @@ mongoose
 .connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-        console.log(`Connected To Db`)
-}
-)
-.catch((err) => console.log(err + " can't connect to db"));
+})
+const db = mongoose.connection
+db.on('error',(err)=>console.log("Can't Connect To Db",err))
+db.once('open',()=>console.log("Connected To Db"))
+
+app.use(express.json())
+app.use(cors())
+app.use('/', postsRoutes)
+app.use(bodyParser.urlencoded({ extended: true }));
